d58bf25866bfdd4aa300993367dd923af09857b7
Shahnawaz Akhtar <114250567+akh1ar@users.noreply.github.com>
2025-01-17 21:40:43 +0530

    Add files via upload

---
+++ in_memory.cpp
@@ -0,0 +1,19 @@
+/*A 2D array is represented as a contiguous block of memory cells(linearly).
+ 1 2 3
+ 4 5 6
+ 7 8 9
+
+ row major
+  element : 1 2 3 4 5 6 7 8 9
+ address:   1000 , 1004, 1008, 1012, 1016, 1020, 1024, 1028, 1032.
+
+ column major
+  element: 1 4 7 2 5 8 3 6 9
+  Address: 100, 104, 108.......132.
+*/
+#include<iostream>
+using namespace std;
+int main(){
+
+    return 0;
+}
